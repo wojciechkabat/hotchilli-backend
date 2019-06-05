3944c41d2724c529fc6498143b12186caaddf2ca
Wojciech Kabat <wojtek.kabat@gmail.com>
2019-06-05 19:49:10 +0200

    fixed auth filters for guests

--- src/main/kotlin/pl/wojciechkabat/hotchilli/controllers/VotingController.kt
+++ src/main/kotlin/pl/wojciechkabat/hotchilli/controllers/VotingController.kt
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.*
 import pl.wojciechkabat.hotchilli.dtos.VoteDto
 import pl.wojciechkabat.hotchilli.entities.User
 import pl.wojciechkabat.hotchilli.services.VoteService
+import java.security.Principal
 import java.time.LocalDate
 import java.util.*
 import kotlin.collections.ArrayList
@@ -14,7 +15,7 @@ class VotingController(
         val voteService: VoteService
 ) {
     @PostMapping("/voting")
-    fun postVote(@RequestBody voteDto: VoteDto) {
+    fun postVote(@RequestBody voteDto: VoteDto, principal: Principal) {
         val currentUser = User(Random().nextLong(), "currentUser", "asdsad", LocalDate.now(), ArrayList(), ArrayList(), "Adsa")
         voteService.persistVote(voteDto, currentUser)
     }
